e989d6de28902f3d2f21a7d8db9fe23b93b5a604
Xine003 <108078507+Xine003@users.noreply.github.com>
2025-02-09 01:54:42 +0800

    Added Edit Schedule (Settings)
    HOW THE EDIT SCHEDULE WORKS
    - The Edit Schedule contains Input Fields and Schedule Table
    - The schedule table will display all of the schedule of the users
    - Clicking a row in the Schedule Table will populate all of the inputs that you can find in the Schedule Table
    - Update Changes button will refresh the Table Schedule after the user is done editing.

--- Assets/Resources/Scripts/SettingsPage/SettingsController.cs
+++ Assets/Resources/Scripts/SettingsPage/SettingsController.cs
@@ -10,4 +10,9 @@ public class SettingsController : MonoBehaviour
         SceneManager.LoadScene("ProfilePage");
     }
 
+    public void LoadEditSchedule() 
+    {
+        SceneManager.LoadScene("EditSchedulePage");
+    }
+
 }
