4531783c72502816aad3602f4193b8e3c54c14bc
jaceey <carataojacelyn145@gmail.com>
2025-03-11 14:28:45 +0800

    Save local changes

--- Assets/Resources/Scripts/OfflineMapController.cs
+++ Assets/Resources/Scripts/OfflineMapController.cs
@@ -1,53 +1,72 @@
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OfflineMapController : MonoBehaviour
 {
-    public GameObject panel;
-    public GameObject panel2;
-    public GameObject panel3;
+    public GameObject panel;   // Main panel (Campus Panel 1)
+    public GameObject panel2;  // Main panel (Campus Panel 2)
+    public GameObject panel3;  // Main panel (Campus Panel 3)
 
-public Dropdown OfflineMapDropdown;
+    public Dropdown OfflineMapDropdown;  // First dropdown (Campus selection)
+    public Dropdown FloorCampusDropdown; // Second dropdown (Floor selection)
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] floorPanels; // Array of floor panels (Ground, 1st, 2nd, etc.)
+
     void Start()
     {
-          OfflineMapDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+        OfflineMapDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+        FloorCampusDropdown.onValueChanged.AddListener(OnFloorDropdownValueChanged);
+        FloorCampusDropdown.gameObject.SetActive(false); // Hide initially
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDropdownValueChanged(int index)
     {
-        
-    }
- public void OnDropdownValueChanged(int index)
-    {
-        // Get the selected value
         string selectedOption = OfflineMapDropdown.options[index].text;
         Debug.Log("Selected: " + selectedOption);
 
-        // Example: Perform different actions based on the selected value
+        // Show/Hide Campus Panels
         switch (index)
         {
-            case 0:
-            panel.SetActive (true);
-            panel2.SetActive (false);
-            panel3.SetActive (false);
-                Debug.Log("Option 1 selected");
+            case 0: // Panel 1 (Enables Floor Dropdown)
+                panel.SetActive(true);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                FloorCampusDropdown.gameObject.SetActive(true); // Show floor dropdown
                 break;
-            case 1:
-            panel.SetActive (false);
-            panel2.SetActive (true);
-            panel3.SetActive (false);
-                Debug.Log("Option 2 selected");
+            case 1: // Panel 2 (Hides Floor Dropdown)
+                panel.SetActive(false);
+                panel2.SetActive(true);
+                panel3.SetActive(false);
+                FloorCampusDropdown.gameObject.SetActive(false); // Hide floor dropdown
+                HideAllFloorPanels();
                 break;
-            case 2:
-             panel.SetActive (false);
-            panel2.SetActive (false);
-            panel3.SetActive (true);
-                Debug.Log("Option 3 selected");
+            case 2: // Panel 3 (Hides Floor Dropdown)
+                panel.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(true);
+                FloorCampusDropdown.gameObject.SetActive(false); // Hide floor dropdown
+                HideAllFloorPanels();
                 break;
+        }
+    }
+
+    public void OnFloorDropdownValueChanged(int index)
+    {
+        // Hide all floor panels first
+        HideAllFloorPanels();
+
+        // Show the selected floor panel
+        if (index >= 0 && index < floorPanels.Length)
+        {
+            floorPanels[index].SetActive(true);
+        }
+    }
+
+    private void HideAllFloorPanels()
+    {
+        foreach (GameObject floorPanel in floorPanels)
+        {
+            floorPanel.SetActive(false);
         }
     }
 }
